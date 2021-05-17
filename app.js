59a45e6005b2c332461cee8d06de96a1c8c7b0a8
EmpujateEste <32001870+EmpujateEste@users.noreply.github.com>
2021-05-17 18:50:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var imgs = document.querySelectorAll(".imgs");
+
+var json;
+
+fetch("https://api.reddit.com/r/cats.json").then(res => res.json()).then(j => json = j).then( () => {
+
+var images = json.data.children;
+
+for(let i = 0; imgs.length; i++){
+
+    imgs[i].setAttribute("src", images[i].data.url);
+
+}
+
+});
