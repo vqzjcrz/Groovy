42b32ad14ad68f64d5504cece5e4c12474b7fb38
EmpujateEste <32001870+EmpujateEste@users.noreply.github.com>
2021-05-17 19:01:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var imgs = document.querySelectorAll(".imgs");
 
 var json;
 
-fetch("https://api.reddit.com/r/uhdnsfw/random.json").then(res => res.json()).then(j => json = j).then( () => {
-
+fetch("https://api.reddit.com/r/UHDnsfw/random.json").then(res => res.json()).then(j => json = j).then( () => {
+console.log(json);
 var images = json.data.children;
-
+console.log(images);
 for(let i = 0; imgs.length; i++){
 
     imgs[i].setAttribute("src", images[i].data.url);
