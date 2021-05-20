b61b84d2be589b05668f144724171df73d828059
EmpujateEste <jeremycclassof2017@gmail.com>
2021-05-19 22:51:56 -0400

    menu functionality

--- public/app.js
+++ public/app.js
@@ -1,21 +1,50 @@
-var arr;
+document.addEventListener('DOMContentLoaded', () => {
 
-fetch("./public/images").then(res => res.json()).then(r => arr = r).then( () => {
-var elements = document.querySelectorAll(".imgs");
-console.log(arr);
-
-function shuffleArray(a) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-var array = shuffleArray(arr);
+var elements = document.querySelectorAll('.nav-items');
 
 for(let i = 0; i < elements.length; i++){
-elements[i].setAttribute("src" , `./public/images/${array[i]}`);
+    elements[i].addEventListener('click', () => alert('This is not yet functional.'))
 }
+
+document.querySelector('.menu-bttn').addEventListener('click', () => {
+
+    var menu = document.querySelector('.nav');
+
+    if(menu.style.transform == "translateY(0%)"){
+        menu.style.transform = "translateY(-100%)"
+    }else{
+        menu.style.transform = "translateY(0%)"
+    }
+
+})
+
+
+var array;
+
+fetch("./public/data.json").then(res => res.json()).then(j => array = j).then(() => {
+
+    console.log(array);
+
+    var elements = document.querySelectorAll('.imgs');
+
+    var shuffle = [];
+
+    for(let i = 0; i < elements.length; i++){
+        var url = array[Math.floor(Math.random()*array.length)];
+
+        if(shuffle.includes(url)){
+            i--;
+        }else{
+            shuffle.push(url);
+        }
+
+    }
+
+    for(let i = 0; i < elements.length; i++){
+        elements[i].setAttribute('src', `./public/${shuffle[i]}`);
+        console.log(shuffle[i]);
+    }
+
+});
 
 });
